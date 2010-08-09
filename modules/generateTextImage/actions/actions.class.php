79aa464ddf5b28dc265028784973015dba364a1c
matt <matt@a13554a6-09ca-47db-9463-62a000a0b514>
2010-08-09 09:50:52 +0000

    Set a Content-Disposition header on image output with a generated filename and file extension.
    git-svn-id: http://fs1/svn/tuiTextImageGeneratorPlugin/trunk@8857 a13554a6-09ca-47db-9463-62a000a0b514

--- modules/generateTextImage/actions/actions.class.php
+++ modules/generateTextImage/actions/actions.class.php
@@ -4,27 +4,46 @@ class generateTextImageActions extends sfActions
 {
   public function executeIndex(sfWebRequest $request)
   {
+    $text       = $request->getParameter('text');
     $text_image = $request->getParameter('image_name');
-    $settings = sfConfig::get('app_textimage_' . $text_image);
+    $settings   = sfConfig::get('app_textimage_' . $text_image);
+    $filename   = sprintf('%08X', crc32($text_image.serialize($settings).$text));
+    $mime_type  = isset($settings['mime_type']) ? $settings['mime_type'] : 'image/gif';
 
     $this->forward404Unless($settings);
+
+    switch(strtolower($mime_type))
+    {
+      case 'image/gif':
+        $filename .= '.gif';
+        break;
+
+      case 'image/png':
+        $filename .= '.png';
+        break;
+      
+      case 'image/jpeg':
+        $filename .= '.jpg';
+        break;
+    }
+    
 
     // Use the callback function/method instead, if set
     if (isset($settings['callback']))
     {
-      $mime_type = isset($settings['mime_type']) ? $settings['mime_type'] : 'image/gif';
       $this->getResponse()->setContentType($mime_type);
+      $this->getResponse()->setHttpHeader('Content-Disposition', 'filename='.$filename);
       
       $parameters = isset($settings['callbackoptions']) ? $settings['callbackoptions'] : array();
       
       try
       {
-        return $this->renderText( call_user_func($settings['callback'], $request->getParameter('text'), $parameters) );
+        return $this->renderText( call_user_func($settings['callback'], $text, $parameters) );
       } catch (Exception $e)
       {
         $response = $this->getResponse();
         $response->setContentType('image/gif');
-        // $response->setContent();
+        $response->setHttpHeader('Content-Disposition', 'filename='.$filename);
         return $this->renderText(textImageGenerator::generateTextImage($e->getMessage()));
       }
       
@@ -32,10 +51,7 @@ class generateTextImageActions extends sfActions
 
 
     try{
-      $image =  textImageGenerator::generateTextImage(
-        $request->getParameter('text'),
-        $settings
-      );
+      $image = textImageGenerator::generateTextImage($text, $settings);
     } catch (Exception $e)
     {
       $response = $this->getResponse();
@@ -46,6 +62,8 @@ class generateTextImageActions extends sfActions
     
     $response = $this->getResponse();
     $response->setContentType('image/gif');
+    $response->setHttpHeader('Content-Disposition', 'filename='.$filename);
+    
     return $this->renderText($image);
 
   }
